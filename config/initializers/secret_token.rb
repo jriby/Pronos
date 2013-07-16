2a48060adbe9fa21741e16a305afa932169c61f4
jriby <jriby@jriby-laptop.(none)>
2013-07-16 17:35:51 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ProjetPronos::Application.config.secret_key_base = 'cfa4b3c9ac4269763f4b4e9626af1f5c17a767058f9a5be23eda7d61aa12dc55e3a2df96f23a2aaf4a45ccb4ab04e4c7139651454ee33900597481cbfee8b9a5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ProjetPronos::Application.config.session_store :cookie_store, key: '_Projet_pronos_session'
